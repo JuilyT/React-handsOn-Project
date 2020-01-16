331761f7ace21fda8513667781cc2285c6984d9c
JuilyT <juilythakur91@gmail.com>
2020-01-16 17:50:44 +0530

    App with redux-thunk

---
+++ blog-posts-redux-thunk/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Postlist from './postlist';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Postlist/>
+            </div>
+        );
+    }
+}
+export default App;
